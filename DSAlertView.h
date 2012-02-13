a81311c4994c1292a644a05f6d33ba7a82faaffd
Devin Snipes <devinsnipes@me.com>
2012-02-12 18:19:37 -0600

    first commit

---
+++ DSAlertView.h
@@ -0,0 +1,15 @@
+//
+//  DSAlertView.h
+//  
+//
+//  Created by Devin Snipes on 1/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface DSAlertView : UIAlertView {
+    
+}
+-(void)setBackgroundColor:(UIColor *)BG withStrokeColor:(UIColor *) setStroke;
+@end
